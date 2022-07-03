0c520f2cd699a7d163fc5c24214ec995e3d1d784
jisooyu <jisooyu@hotmail.com>
2022-07-03 17:05:04 +0900

    adopting async/await in uploadImage

---
+++ frontend/src/components/uploadImage-old-way.js
@@ -0,0 +1,35 @@
+const S3 = require("aws-sdk/clients/s3");
+
+function uploadImage(file) {
+  const bucketName = process.env.REACT_APP_AWS_BUCKET_NAME;
+  const region = process.env.REACT_APP_AWS_REGION;
+  const accessKeyId = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
+
+  const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+  });
+
+  return new Promise((resolve, reject) => {
+    if (!file) {
+      reject("Invalid file");
+    }
+    s3.upload(
+      {
+        Key: file.name,
+        Bucket: bucketName,
+        Body: file,
+      },
+      (err, data) => {
+        if (err) {
+          console.log("Error! ", err);
+          return reject("There was an error uploading your image: ");
+        }
+        resolve(data.Location);
+      }
+    );
+  });
+}
+export default uploadImage;
--- frontend/src/components/uploadImage.js
+++ frontend/src/components/uploadImage.js
@@ -1,6 +1,6 @@
 const S3 = require("aws-sdk/clients/s3");
 
-function uploadImage(file) {
+const uploadImage = async (file) => {
   const bucketName = process.env.REACT_APP_AWS_BUCKET_NAME;
   const region = process.env.REACT_APP_AWS_REGION;
   const accessKeyId = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
@@ -11,24 +11,13 @@ function uploadImage(file) {
     accessKeyId,
     secretAccessKey,
   });
-  return new Promise((resolve, reject) => {
-    if (!file) {
-      reject("Invalid file");
-    }
-    s3.upload(
-      {
-        Key: file.name,
-        Bucket: bucketName,
-        Body: file,
-      },
-      function (err, data) {
-        if (err) {
-          console.log("Error! ", err);
-          return reject("There was an error uploading your image: ");
-        }
-        resolve(data.Location);
-      }
-    );
-  });
-}
+  const result = await s3
+    .upload({
+      Key: file.name,
+      Bucket: bucketName,
+      Body: file,
+    })
+    .promise();
+  return result.Location;
+};
 export default uploadImage;
